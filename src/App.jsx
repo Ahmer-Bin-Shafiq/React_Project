4ec52c50c5eb4308a2fbac9b08e32933d7a6412b
Ahmer Bin Shafiq <iamahmar03@gmail.com>
2025-10-18 09:58:50 +0500

    My first Commit
    My first Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import IncreaseCounter from "./Test"
+const App = () => {
+  return (
+    <div>
+      {/* <button id='clickcheck' onClick={()=>console.log('YOu clicked')}>Click me</button> */}
+      <IncreaseCounter />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Test.jsx
@@ -0,0 +1,6 @@
+function IncreaseCounter()
+{
+  return (<button id="TestButton" onClick={()=>console.log('YOu clicked')}>Click me</button>)
+}
+
+export default IncreaseCounter;
